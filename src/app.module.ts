c249be4775d452e18a947c74a817d8e6932eadd1
Jose Henriquez <jose.henriquez2101@alumnos.ubiobio.cl>
2025-09-22 15:40:07 -0300

    cambios a main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { RecetasModule } from './recetas/recetas.module';
 
 @Module({
   imports: [ProductosModule, UsuariosModule, RecetasModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
