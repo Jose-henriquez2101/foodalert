a643bbfc3e79d7978f4a66ff3831673b790c05fe
Natalia <natalia.carrasco2101@alumnos.ubiobio.cl>
2025-09-22 16:54:01 -0300

    Actualizacion receta

--- src/productos/dto/update-producto.dto.ts
+++ src/productos/dto/update-producto.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductoDto } from './create-producto.dto';
+import { IsOptional, IsString, IsUUID } from "class-validator";
 
-export class UpdateProductoDto extends PartialType(CreateProductoDto) {}
+
+export class UpdateProductoDto {
+    @IsUUID()
+    @IsOptional()
+    @IsString()
+    readonly id?: string;
+    
+    @IsOptional()
+    @IsString()
+    readonly nombre?: string;
+
+    @IsOptional()
+    @IsString()
+    readonly descripcion?: string;
+
+    @IsOptional()
+    @IsString()
+    readonly ingredientes?: string;
+
+}
--- src/recetas/dto/create-receta.dto.ts
+++ src/recetas/dto/create-receta.dto.ts
@@ -1,1 +1,14 @@
-export class CreateRecetaDto {}
+import { IsString } from "class-validator";
+
+export class CreateRecetaDto {
+
+    @IsString()
+    readonly nombre: string;
+    
+    @IsString()
+    readonly descripcion: string;
+
+    @IsString()
+    readonly ingredientes: string;
+
+}
