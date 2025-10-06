a45518ef9c924bdcf52e6c5790b8eda1b1937768
Jose Henriquez <jose.henriquez2101@alumnos.ubiobio.cl>
2025-10-06 19:04:27 -0300

    cambios menores

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -2,7 +2,7 @@ import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
 
 @Entity()
 export class Usuario {
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
